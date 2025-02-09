18784e225cf39ed1d9939288a225b9c287f103c1
“akshaymandale” <akshay.mandale@gmail.com>
2025-02-09 21:56:41 +0530

    Changes for search button added on usermanagement

--- views/includes/header.php
+++ views/includes/header.php
@@ -10,6 +10,8 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Unlock Your Skills</title>
+    <!-- Font Awesome for icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="public/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/style.css">
     <script src="public/js/jquery.min.js"></script>
